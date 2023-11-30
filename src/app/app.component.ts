80ad59fd449ec44fe88d1728c8deed2960a5353d
Kaylee Mehlhorn <mehlhorn17@gmail.com>
2023-12-01 12:14:04 +1300

    world.svg added, along with an html table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { WorldMapComponent } from "./world-map/world-map.component";
+
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, WorldMapComponent]
 })
 export class AppComponent {
-  title = 'world-map-project';
+  title = 'world-map';
 }
