eccab29340b264dd2d802e51b2823ade610961ad
Kaylee Mehlhorn <mehlhorn17@gmail.com>
2023-12-06 13:18:17 +1300

    fix more eslint issue

--- src/app/components/world-map/world-map.component.ts
+++ src/app/components/world-map/world-map.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-world-map',
   standalone: true,
   imports: [],
   templateUrl: './world-map.component.html',
-  styleUrl: './world-map.component.scss',
+  styleUrl: './world-map.component.scss'
 })
 export class WorldMapComponent {
 }
